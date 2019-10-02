f2b897b7786013addf26e7e474b37592485e28e9
unknown <you@example.com>
2019-10-02 15:57:55 +0300

    fix bug aplication style page. add script js - test

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,33 +1,41 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
+  $(function() {
+    var location = window.location.href;
+    console.log(location);
+    var cur_url = location.split("/").pop();
+    console.log(cur_url);
 
-    //searc-btn
-    
-    let btn_search = document.querySelector('.search__btn');
-    
-        btn_search.addEventListener("click", () => {
-        
-            document.querySelector('.search__formwrap').classList.toggle('is-active');
-        
-        });
-    
-    //burger-menu
-    
-    let burgerButton = document.querySelector(".burger-menu__button");
-    
-        burgerButton.addEventListener("click",() => {
-        
-            let burgerMenu = document.querySelector(".burger-menu");
+    $(".header__list-nav a").each(function() {
+      var link = $(this).attr("href");
 
-                burgerMenu.classList.toggle("is-active");
+      if (cur_url == link) {
+        $(this).addClass("current");
+      }
+    });
+  });
+  //searc-btn
 
-            let list = document.querySelector(".header__list-nav");
+  let btn_search = document.querySelector(".search__btn");
 
-                list.classList.toggle("is-active");
+  btn_search.addEventListener("click", () => {
+    document.querySelector(".search__formwrap").classList.toggle("is-active");
+  });
 
-            let burgerOverlay =  document.querySelector(".burger-menu__overlay");
+  //burger-menu
 
-                burgerOverlay.classList.toggle("is-active");
-        
-        });
-    
+  let burgerButton = document.querySelector(".burger-menu__button");
+
+  burgerButton.addEventListener("click", () => {
+    let burgerMenu = document.querySelector(".burger-menu");
+
+    burgerMenu.classList.toggle("is-active");
+
+    let list = document.querySelector(".header__list-nav");
+
+    list.classList.toggle("is-active");
+
+    let burgerOverlay = document.querySelector(".burger-menu__overlay");
+
+    burgerOverlay.classList.toggle("is-active");
+  });
 });
